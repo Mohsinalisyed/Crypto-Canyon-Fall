35416eebd76c8cfef2917527458e7472e58afcce
Mohsin512 <mohsin.syed@ideofuzion.com>
2024-07-04 18:12:07 +0500

    recommit responsive ness

--- src/lib/MainHeading.tsx
+++ src/lib/MainHeading.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Mainheading = styled.h2`
  color:#fff;
  font-size:52px;
- margin:0
+ margin:0;
  @media (max-width: 600px) {
  font-size:32px;
   }
