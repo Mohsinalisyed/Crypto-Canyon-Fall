388145d77c4e7409f85c994562c53ffbcea63254
Mohsin512 <mohsin.syed@ideofuzion.com>
2024-07-04 17:18:35 +0500

    recommit

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,6 +3,15 @@ import { Mainheading, Text } from '../lib'
 import logo  from '../assets/icon.png'
 import styled from 'styled-components'
 const Main = () => {
+    const handleInstall = () => {
+        const apkUrl = '/Cryptocanyon.apk';
+        const downloadLink = document.createElement('a');
+        downloadLink.href = apkUrl;
+        downloadLink.download = '';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    };
     return (
         <div style={{ background: "black" }}> 
             <MainContainer>
@@ -33,15 +42,15 @@ const Main = () => {
                         </div>
                         <div className='btnwrapper'>
                             <div className='shareWrapper'>
-                                <button className='installbtn'>Install</button>
-                                <div className='shareOption'>
+                                <button className='installbtn' onClick={handleInstall}>Install</button>
+                                <div className='shareOption' onClick={handleInstall}>
                                     <svg className="f70z8e" width="24" height="24" viewBox="0 0 24 24" fill='#6df378'>
                                         <path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.54.5 1.25.81 2.04.81 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L8.04 9.81C7.5 9.31 6.79 9 6 9c-1.66 0-3 1.34-3 3s1.34 3 3 3c.79 0 1.5-.31 2.04-.81l7.12 4.16c-.05.21-.08.43-.08.65 0 1.61 1.31 2.92 2.92 2.92 1.61 0 2.92-1.31 2.92-2.92s-1.31-2.92-2.92-2.92z">
                                         </path></svg>
-                                    <p style={{paddingLeft:"8px"}}>Share</p>
+                                    <p style={{paddingLeft:"8px"}} >Share</p>
                                     
                                 </div>
-                                <div className='shareOption'>
+                                <div className='shareOption' onClick={handleInstall}>
                                     <svg className="XkAcee" width="24" height="24" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M7 3H17C18.1045 3 19 3.8955 19 5V21L12 18L5 21L5.01075 5C5.01075 3.8955 5.8965 3 7 3ZM12 15.824L17 18V5H7V18L12 15.824ZM13 7V9H15V11H13V13H11V11H9V9H11V7H13Z" fill='#6df378'></path></svg>
                                     <p style={{ paddingLeft: "8px" }}>Add to wishlist</p>
                                 </div>
