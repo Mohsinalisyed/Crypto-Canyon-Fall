a74cb5ab04762ac94300436c9f4639d41ab0fc56
Mohsin512 <mohsin.syed@ideofuzion.com>
2024-07-05 16:14:25 +0500

    recommit url of images

--- src/gamesdata.ts
+++ src/gamesdata.ts
@@ -1,19 +1,14 @@
-import redEggLogo from './assets/RedEggRolling/icon.jpeg'
-import freekyLogo from './assets/Freeky Fall/freekyfall.webp'
-import croptoTile from './assets/Crypto Tiles/cryptotiles.webp'
-import cryptoBlock from './assets/Crypto Block/crypto blocks.webp'
-import blockShooter from './assets/Block Shooter Puzzle Game/Logo.png'
-import redEggSS1 from './assets/RedEggRolling/1.jpg'
-import redEggSS2 from './assets/RedEggRolling/2.jpg'
-
 
 export const GamesData = [
     {
         name: 'Red Egg Rolling',
-        logo: redEggLogo,
+        logo: 'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Egg%20Rolling%20Game%20%20BTC%20Rewards%2Ficon.jpeg?alt=media&token=0864e65e-c30a-430d-8e82-ea31fc47fa45',
         screen_shot: [
-            redEggSS1,
-            redEggSS2,
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Block%20Shooter%20Puzzle%20Game%2F1.jpg?alt=media&token=00282145-4bf0-4a89-97ee-29d09276eeca',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Block%20Shooter%20Puzzle%20Game%2F2.jpg?alt=media&token=43f1d026-782c-4360-8275-7e4726c7539c',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Block%20Shooter%20Puzzle%20Game%2F3.jpg?alt=media&token=19664c83-cccd-42a5-8bc3-0fa106b79a69',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Block%20Shooter%20Puzzle%20Game%2F4.jpg?alt=media&token=918e0834-bd41-4c48-bd86-892516ebab5c',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Block%20Shooter%20Puzzle%20Game%2F5.jpg?alt=media&token=d8ed0213-62d6-4218-a814-67ec9fd65c91',
         ],
         about: "The mighty Egg finds itself at the middle of dinosaur society. Its father is a dinosaur clan chief and always support it to grow slowly and hatch. But the egg intend to look for its mother.",
         updated_on: 'Jun 12, 2024',
@@ -22,7 +17,7 @@ export const GamesData = [
     {
         name: 'Block Shooter',
         nameLine2: 'Puzzle Game',
-        logo: blockShooter,
+        logo: 'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Block%20Shooter%20Puzzle%20Game%2FIcon.3.png?alt=media&token=6a6f069f-55a4-4e09-b147-e0a29c3cd1db',
         apk_file:'/public_assets/SmashHero.apk',
         screen_shot: [
             'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Block%20Shooter%20Puzzle%20Game%2F1.jpg?alt=media&token=00282145-4bf0-4a89-97ee-29d09276eeca',
@@ -40,11 +35,15 @@ export const GamesData = [
     {
         name: 'Crypto Block',
         nameLine2: 'Puzzle Challenge',
-        logo: cryptoBlock,
+        logo: 'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Blocks%20Puzzle%20Challenge%2Fcrypto%20blocks.webp?alt=media&token=eec41cfa-8938-4ee0-a1a9-9eb970ad4b92',
         apk_file:"/public_assets/Crypto Blocks Puzzle Challenge_APK.apk",
         screen_shot: [
-            redEggSS1,
-            redEggSS2,
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Blocks%20Puzzle%20Challenge%2FblocksSS1.webp?alt=media&token=992bdd4d-6095-4c72-9705-57a774eed426',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Blocks%20Puzzle%20Challenge%2FblocksSS2.webp?alt=media&token=1838b159-6702-4387-91b2-6c925d9f4a72',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Blocks%20Puzzle%20Challenge%2FblocksSS3.webp?alt=media&token=6ef21ec6-2a3c-45b1-b346-5a2b18fef9c3',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Blocks%20Puzzle%20Challenge%2FblocksSS5.webp?alt=media&token=8051669c-f53d-4ef9-b833-f6152b7a1be7',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Blocks%20Puzzle%20Challenge%2FblocksSS6.webp?alt=media&token=f704a213-edb4-4b9f-bcd9-8ac55f8ba9ac',
+        
         ],
         about: "Do you want to play games and win Bitcoin? Play Crypto Blocks: Earn Real BTC to be entered into a free game for a chance at winning a Bitcoin prize! You're right on time since the crypto blocks have returned. Become the first bitcoin holder! Prepare to pop the matching blocks and advance through each level. Enjoy playing the free-to-play and play-to-earn game Crypto Blocks. A delightful pop crypto block game that presents difficulties at every turn. To earn bitcoin, match the blocks and pop them. Bitcoin-generating games that you may play for free online",
         updated_on: "Jun 27, 2024",
@@ -53,11 +52,15 @@ export const GamesData = [
     {
         name: 'Crypto Tiles',
         nameLine2: 'Earn Real Bitcoin',
-        logo: croptoTile,
+        logo: 'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Tiles%20Earn%20Real%20Bitcoin%2Funnamed.webp?alt=media&token=26cedbac-eb6f-4ca8-8689-60d094a86495',
         apk_file:"/public_assets/Crypto Tiles.apk",
         screen_shot: [
-            redEggSS1,
-            redEggSS2,
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Tiles%20Earn%20Real%20Bitcoin%2Funnamed%20(2).webp?alt=media&token=0ee74aa0-4a8d-4b5e-8b3b-cfc39cea309e',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Tiles%20Earn%20Real%20Bitcoin%2Funnamed%20(3).webp?alt=media&token=90c4238d-8852-4057-b583-4e2b9716e1b6',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Tiles%20Earn%20Real%20Bitcoin%2Funnamed%20(4).webp?alt=media&token=062201f9-3abf-4f2b-809c-3a6c0c270b58',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Tiles%20Earn%20Real%20Bitcoin%2Funnamed%20(5).webp?alt=media&token=3b5aeac0-84ed-4087-9e10-58ac80ac6304',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Crypto%20Tiles%20Earn%20Real%20Bitcoin%2Funnamed%20(6).webp?alt=media&token=dd5d32b1-9298-422d-a80d-477427d2957f'
+
         ],
         about: "BTC Earn crypto: coin base earning app.",
         aboutLine2: "Start playing Crypto Tiles today. A legendary puzzle game loved by millions of players around the world. Play & Earn concept used in the game. The more you play the more you earn. Break the tiles and earn bitcoin. Switch and match tiles in this puzzle adventure to progress to the next level for that winning feeling! Solve puzzles with quick thinking and smart moves.",
@@ -67,11 +70,14 @@ export const GamesData = [
     },
     {
         name: 'Freeky Fall',
-        logo: freekyLogo,
+        logo: 'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Freeky%20Fall%20%20Ball%20Game%2Ffreekyfall.webp?alt=media&token=b03151cd-673d-4dbe-8b42-d604ca409e49',
         apk_file:'/public_assets/Freeky Fall_APK.apk',
         screen_shot: [
-            redEggSS1,
-            redEggSS2,
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Freeky%20Fall%20%20Ball%20Game%2F1_SS.webp?alt=media&token=e20df477-9029-4e60-b70b-b33d35448fe7',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Freeky%20Fall%20%20Ball%20Game%2F2_SS.webp?alt=media&token=03023965-8917-4380-80cb-6515d2a527d8',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Freeky%20Fall%20%20Ball%20Game%2F3_SS.webp?alt=media&token=6685994d-a872-4746-b27b-603b6984d135',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Freeky%20Fall%20%20Ball%20Game%2F4_SS.webp?alt=media&token=18b18ae7-879c-4d2b-95bd-7df4be8a53bb',
+            'https://firebasestorage.googleapis.com/v0/b/relaxeum-8755b.appspot.com/o/Freeky%20Fall%20%20Ball%20Game%2F5_SS.webp?alt=media&token=ceb5639c-f4c8-4a57-8b14-b678e75f8002',
         ],
         about: ''
     }
