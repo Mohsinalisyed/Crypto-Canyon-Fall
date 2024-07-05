f6a03ca5147701346b2cc0160a4fa8ee3ca0113c
Mohsin512 <mohsin.syed@ideofuzion.com>
2024-07-05 17:36:46 +0500

    lazy loading

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import Main from '../components/Main'
-import Slider from '../components/Slider'
-import ContentSection from '../components/ContentSection'
-import FooterSection from '../components/FooterSection'
+import React, { Suspense } from 'react';
+
+// Lazy load components
+const LazyMain = React.lazy(() => import('../components/Main'));
+const LazySlider = React.lazy(() => import('../components/Slider'));
+const LazyContentSection = React.lazy(() => import('../components/ContentSection'));
+const LazyFooterSection = React.lazy(() => import('../components/FooterSection'));
 
 const Home = () => {
   return (
-      <div>
-          <Main />
-          <Slider />
-          <ContentSection />
-          <FooterSection />
+    <div>
+      <Suspense fallback={<div style={{height:"100vh" , display:"flex" , alignItems:"center" ,justifyContent:"center"}}>Loading...</div>}>
+        <LazyMain />
+        <LazySlider />
+        <LazyContentSection />
+        <LazyFooterSection />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
